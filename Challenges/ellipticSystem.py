4b199f4fac46696855fe959641c19f4e833df7c0
Jahirul Islam <jahirul.iit5th@gmail.com>
2017-12-10 23:36:09 +0600

    Update ellipticSystem.py

--- Challenges/ellipticSystem.py
+++ Challenges/ellipticSystem.py
@@ -13,6 +13,19 @@
 #| B/2  C  E/2 |
 #| D/2 E/2  F  |
 # 
+#https://math.stackexchange.com/questions/163920/how-to-find-an-ellipse-given-five-points
+#https://math.stackexchange.com/questions/247332/area-of-an-ellipse
+#import numpy as np
+#def ellipticSystem(points):
+#
+#    M = np.array([ [x*x,x*y,y*y,x,y,1.] for x,y in zip(points[::2],points[1::2])])
+#    A,B,C,D,E,F =  [np.linalg.det(M[:,[ i!=k for i in range(6)]])*(-1)**k for k in range(6)]
+#    I1= A+C
+#    I2= (A-C)**2+B*B
+#    I3= D*D+E*E
+#    I4= (A-C)*(D*D-E*E)+2*D*E*B
+#    I5 = F
+#    return abs(np.pi*(I1*I3-I4-2*(I1**2-I2)*I5)/(I1**2-I2)**1.5)
 from itertools import permutations
 import math
 def sin(check):
@@ -38,27 +51,18 @@ def ellipticSystem(points):
     dis = B*B - 4*A*C
     det = A*C*F + B*D*E/4.0 - A*E*E/4.0 - B*B*F/4.0 - C*D*D/4.0
     a = 2*math.sqrt(2*det/dis/((A+C)-math.sqrt((A-C)*(A-C)+B*B)))
- #https://math.stackexchange.com/questions/163920/how-to-find-an-ellipse-given-five-points
-#https://math.stackexchange.com/questions/247332/area-of-an-ellipse
-#import numpy as np
-#def ellipticSystem(points):
-#
-#    M = np.array([ [x*x,x*y,y*y,x,y,1.] for x,y in zip(points[::2],points[1::2])])
-#    A,B,C,D,E,F =  [np.linalg.det(M[:,[ i!=k for i in range(6)]])*(-1)**k for k in range(6)]
-#    I1= A+C
-#    I2= (A-C)**2+B*B
-#    I3= D*D+E*E
-#    I4= (A-C)*(D*D-E*E)+2*D*E*B
-#    I5 = F
-#    return abs(np.pi*(I1*I3-I4-2*(I1**2-I2)*I5)/(I1**2-I2)**1.5)
-#same code....  
+    b = 2*math.sqrt(2*det/dis/((A+C)+math.sqrt((A-C)*(A-C)+B*B)))
+    return math.pi*a*b
+
+#same code.... 
+
 import numpy as n
 A,B,C,D,E = n.linalg.solve([[x*x,x*y,y*y,x,y] for x,y in zip(*[iter(eval(dir()[0])[0])]*2)],[1]*5)
 i= 4*A*C-B*B
 return n.pi*2*abs(A*E*E+C*D*D-D*E*B+i)/i**1.5
-    b = 2*math.sqrt(2*det/dis/((A+C)+math.sqrt((A-C)*(A-C)+B*B)))
-    return math.pi*a*b
+    
 #same code.....
+
 from numpy import *
 a,b,c,d,f=linalg.inv([(x*x,x*y,y*y,x,y) for x,y in reshape(eval(dir()[47]),(5,2))]).dot([1]*5)
 q=4*a*c-b*b
