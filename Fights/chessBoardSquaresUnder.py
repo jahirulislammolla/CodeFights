fc7e09094ac089a39bcace5d47b4fd243a897b80
Jahirul Islam <jahirul.iit5th@gmail.com>
2017-12-15 01:19:21 +0600

    Create chessBoardSquaresUnder.py

---
+++ Fights/chessBoardSquaresUnder.py
@@ -0,0 +1,19 @@
+def chessBoardSquaresUnderQueenAttack(a, b):               
+                                                           
+    def go(x, y, dx, dy):                                  
+        if x < 0 or x >= a or y < 0 or y >= b:             
+            return 0                                       
+        return go(x + dx, y + dy, dx, dy) + 1              
+                                                           
+    res = 0                                                
+                                                           
+    for i in range(a):                                     
+        for j in range(b):                                 
+            for dx in range(-1, 2):                        
+                for dy in range(-1, 2):                    
+                    if dx != 0 or dy != 0:                 
+                        #print(go(i,j,dx,dy)/3)            
+                        res +=  (go(i,j,dx,dy)-1)          
+                                                           
+    return res                                             
+print(chessBoardSquaresUnderQueenAttack(2,3))              
