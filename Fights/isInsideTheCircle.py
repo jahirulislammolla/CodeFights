650b2519d63880cd3afa86b34d609fa61307698f
Jahirul Islam <jahirul.iit5th@gmail.com>
2017-12-15 00:47:45 +0600

    Create isInsideTheCircle.py

---
+++ Fights/isInsideTheCircle.py
@@ -0,0 +1,7 @@
+def isInsideTheCircle(xa, ya, xc, yc, rc):                 
+    dist = (xa - xc) * (xa - xc) + (ya - yc) * (ya - yc)   
+    rc *= rc                                               
+    if dist < rc:                                          
+        return True                                        
+    return  False                                          
+                                                           
