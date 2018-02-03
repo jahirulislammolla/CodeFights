b5a1ff4eea1ac6e7246416cf0a9653ce2e05a521
Jahirul Islam <jahirul.iit5th@gmail.com>
2018-02-04 00:14:38 +0600

    Update bfsDistancesUnweightedGraph.java

--- Fights/bfsDistancesUnweightedGraph.java
+++ Fights/bfsDistancesUnweightedGraph.java
@@ -45,3 +45,47 @@ int bfsDistancesUnweightedGraph2(std::vector<std::vector<bool>> matrix, int vert
         }
         return distances;
 }
+int largestFullBinaryTree(int[] parent) {
+
+  class Graph {
+    ArrayList<Integer>[] edges;
+    int maxBinTree;
+
+    Graph(int[] parent) {
+      maxBinTree = 1;
+      edges = new ArrayList[parent.length];
+      for (int i = 0; i < edges.length; i++) {
+        edges[i] = new ArrayList();
+      }
+      for (int i = 1; i < parent.length; i++) {
+        edges[parent[i]].add(i);
+      }
+    }
+
+    int dfs(int v) {
+      int firstMax = -1;
+      int secondMax = -1;
+      for (int u : edges[v]) {
+        int curMax = dfs(u);
+        if (curMax > firstMax) {
+          secondMax = firstMax;
+          firstMax = curMax;
+        } else if (curMax > secondMax) {
+          secondMax = curMax;
+        }
+      }
+      if (secondMax == -1) {
+        return 1;
+      }
+      int result = 1 + firstMax + secondMax ;
+      if (result > maxBinTree) {
+        maxBinTree = result;
+      }
+      return result;
+    }
+  }
+
+  Graph g = new Graph(parent);
+  g.dfs(0);
+  return g.maxBinTree;
+}
