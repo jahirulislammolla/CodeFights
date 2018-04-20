7a9aefc53834d8b57b8665cd9cd0200c56ee8eb1
Jahirul Islam <jahirul.iit5th@gmail.com>
2018-04-20 22:14:40 +0600

    Update electionsWinners.js

--- Fights/electionsWinners.js
+++ Fights/electionsWinners.js
@@ -1,3 +1,14 @@
+def electionsWinners(votes, k):
+    m = max(votes)
+    
+    if k == 0:
+        if votes.count(m) > 1:
+            return 0
+        else:
+            return 1
+    return sum(v+k > m for v in votes)
+
+#....................########........>>>>>>>>>>
 function electionsWinners(votes, k) {
  var currentMaxIndex = 0,
         cntLeaders = 0,
