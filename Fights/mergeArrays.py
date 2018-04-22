0b26a407acc0e5cf87e61edf3acbc1b6885097d3
Jahirul Islam <jahirul.iit5th@gmail.com>
2018-04-22 19:43:31 +0600

    Create mergeArrays.py

---
+++ Fights/mergeArrays.py
@@ -0,0 +1,23 @@
+def mergeArrays(a, b):
+
+    result = []
+
+    i = 0
+    j = 0
+    while i < len(a) and j < len(b):
+        if a[i] < b[j]:
+            result.append(a[i])
+            i += 1
+        else:
+            result.append(b[j])
+            j += 1
+
+    while i < len(a):
+        result.append(a[i])
+        i += 1
+
+    while j < len(b):
+        result.append(b[j])
+        j += 1
+
+    return result
