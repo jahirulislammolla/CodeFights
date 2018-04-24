fc885d2d772272ff7f2343d04f1da1439744fc75
Jahirul Islam <jahirul.iit5th@gmail.com>
2018-04-24 23:43:36 +0600

    Create isDivisibleBy6.py

---
+++ Fights/isDivisibleBy6.py
@@ -0,0 +1,23 @@
+def isDivisibleBy6(inputString):
+
+    digitSum = 0
+    leftBound = ord('0')
+    rightBound = ord('9')
+    answer = []
+    mask = list(inputString)
+    asteriskPos = -1
+
+    for i in range(len(mask)):
+        if (leftBound <= ord(mask[i]) and
+          ord(mask[i]) <= rightBound):
+            digitSum += ord(mask[i]) - leftBound
+        else:
+            asteriskPos = i
+
+    for i in range(10):
+        if (digitSum + i) % 3 == 0:
+            mask[asteriskPos] = chr(leftBound + i)
+            if  (int(mask[len(mask)-1]) - leftBound) % 2==0 :
+                answer.append(''.join(mask))
+
+    return answer
