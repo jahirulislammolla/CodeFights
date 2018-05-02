e1a4924394c80fea8f749dd09a3bc3f4a89b3958
Jahirul Islam <jahirul.iit5th@gmail.com>
2018-05-02 19:37:21 +0600

    Update inversePermutation.py

--- Fights/inversePermutation.py
+++ Fights/inversePermutation.py
@@ -1,5 +1,5 @@
 def inversePermutation(permutation):
     x=[0]*len(permutation)
     for i in range(len(permutation)):
-        x[permutation[i]-1]=I)
+        x[permutation[i]-1]=i+1
     return x
