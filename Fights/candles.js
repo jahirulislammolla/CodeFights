c4adf4753404b4ac7a8ba554f7a1c7b2fbf46b65
Jahirul Islam <jahirul.iit5th@gmail.com>
2018-05-17 15:10:43 +0600

    Update candles.js

--- Fights/candles.js
+++ Fights/candles.js
@@ -1,3 +1,12 @@
 function candles(candlesNumber, makeNew) {
     return candlesNumber + Math.floor((candlesNumber - 1) / (makeNew - 1));
 }
+def candles(candlesNumber, makeNew):
+    burned = 0
+    leftovers = 0
+    while candlesNumber > 0:
+        burned += candlesNumber
+        leftovers += candlesNumber
+        candlesNumber = leftovers // makeNew
+        leftovers %= makeNew
+    return burned
