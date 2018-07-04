4d216e135115bce432ff57c1363fdf5518a310c6
Jahirul Islam <jahirul.iit5th@gmail.com>
2018-07-04 23:16:20 +0600

    Create climbingStairs.py

---
+++ InterviewPractice/DP_basic/climbingStairs.py
@@ -0,0 +1,16 @@
+def climbingStairs(n):
+    b=f(n+1)  
+    return b[0]
+    x=[-1]*n
+    def f(n):
+        if n == 0:
+            return (0, 1)
+        else:
+            a, b = f(n // 2)
+            c = (a * (2 * b - a))%(10**9+7)
+            d = (b * b + a * a)%(10**9+7)
+
+            if n % 2 == 0:
+                return (c, d)
+            else:
+                return (d, c + d)
