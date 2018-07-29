10d4c36c764271a95df6ac9cd8c321afbf84d875
Jahirul Islam <jahirul.iit5th@gmail.com>
2018-07-29 12:41:15 +0600

    Update theJanitor.py

--- Fights/theJanitor.py
+++ Fights/theJanitor.py
@@ -10,3 +10,25 @@ def theJanitor(word):
     for i in x:
         y[i]=x[i][-1]-x[i][0]+1
     return y
+
+def theJanitor(word):
+
+    left = [0] * 26
+    right = [0] * 26
+    was = [0] * 26
+    for i in range(26):
+        left.append(0)
+        right.append(0)
+        was.append(False)
+
+    for i in range(len(word)):
+        c = ord(word[i]) - ord('a')
+        if not was[c]:
+            was[c]=True
+            left[c]=i
+        right[c] = i
+
+    ans = []
+    for i in range(26):
+        ans.append(right[i] - left[i] + 1 if was[i] else 0)
+    return ans
